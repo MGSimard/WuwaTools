f943b8b89a3d23d4e1af1f9f682d811640a5eecb
MGSimard <g.marcgs@gmail.com>
2024-06-15 16:13:34 -0400

    Introduce incorrect character route return

--- src/routes/roster.$resonator.tsx
+++ src/routes/roster.$resonator.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 import charactersData from "../assets/characters.json";
 
 export const Route = createFileRoute("/roster/$resonator")({
@@ -8,6 +8,15 @@ export const Route = createFileRoute("/roster/$resonator")({
 function Resonator() {
   const { resonator } = Route.useParams();
   const charData = charactersData.data.find((char) => char.name.toLowerCase() === resonator);
+
+  if (!charData) {
+    return (
+      <>
+        <h1>Invalid Resonator! ({resonator})</h1>
+        <Link to="/roster">&laquo; Go Back</Link>
+      </>
+    );
+  }
   console.log(charData);
 
   return <div>Character Page: {resonator}</div>;
