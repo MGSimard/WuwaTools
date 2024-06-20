2a3364e04de059a2699efcfed905266aa0e600bc
MGSimard <g.marcgs@gmail.com>
2024-06-19 22:29:31 -0400

    update index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,13 +8,34 @@ function Home() {
   return (
     <>
       <h1>WuwaTools - Work In Progress</h1>
-      <h2>To Do:</h2>
-      <ul>
-        <li>- More features potentially</li>
-        <li>- Updating info in character profiles</li>
-        <li>- Figure out layout for character profile header</li>
-        <li>- Make different icons for Rover element types</li>
-      </ul>
+      <section>
+        <h2>Features:</h2>
+        <ul>
+          <li>- Roster (Character Profiles)</li>
+          <li>- Tier List Creator</li>
+        </ul>
+      </section>
+      <section>
+        <h2>To Do:</h2>
+        <ul>
+          <li>- More features potentially</li>
+          <li>- Updating info in character profiles</li>
+          <li>- Figure out layout for character profile header</li>
+          <li>- Make different icons for Rover element types</li>
+        </ul>
+      </section>
+      <section>
+        <h2>Tech</h2>
+        <ul>
+          <li>- React/Vite SWC</li>
+          <li>- TypeScript</li>
+          <li>- TanStack Router</li>
+          <li>- @dnd-kit/react (Alpha dnd kit rewrite)</li>
+          <li>- FontAwesome</li>
+          <li>- html-to-image</li>
+          <li>- downloadjs</li>
+        </ul>
+      </section>
     </>
   );
 }
