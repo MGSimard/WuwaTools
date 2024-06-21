cca1be194578813e387f2fb83e039f5c9641d6db
MGSimard <g.marcgs@gmail.com>
2024-06-20 22:46:43 -0400

    Taoqi, MillionLint

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import MillionLint from "@million/lint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), MillionLint.vite()],
 });
