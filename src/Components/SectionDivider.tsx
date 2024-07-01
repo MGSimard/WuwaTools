2cacef6eecdf0acdad545b086b3d6f0153fa87e6
MGSimard <g.marcgs@gmail.com>
2024-06-30 22:25:29 -0400

    Divider line decoration

--- src/Components/SectionDivider.tsx
+++ src/Components/SectionDivider.tsx
@@ -1,8 +1,12 @@
+import lineDecor from "../assets/ui/T_VisionRecycle_Star.png";
+
 export const SectionDivider = ({ title }: { title: string }) => {
   return (
     <div className="section-divider">
       <h2>▸ {title}</h2>
-      <div className="sd-line"></div>
+      <div className="sd-line">
+        <img className="sd-line-decor" src={lineDecor} alt="" />
+      </div>
     </div>
   );
 };
